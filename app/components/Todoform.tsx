d3065b61158ee6003d3cd5ce47edd02d1f70325b
Prakash7078 <20bq1a05j5@vvit.net>
2023-09-08 14:37:25 +0530

    changes in form

--- app/components/Todoform.tsx
+++ app/components/Todoform.tsx
@@ -50,8 +50,8 @@ function Todoform({ fetchTasks }: { fetchTasks: () => void }) {
                 <BiSolidAddToQueue />
             </div>
             {open &&
-                <form className="mt-5 shadow-inner bg-white sm:p-10 px-5 py-5 flex flex-col gap-5" onSubmit={handleFormSubmit}>
-                    <div className="flex gap-14">
+                <form className="mt-5 shadow-inner bg-white sm:p-10 px-5 py-5 flex flex-col gap-5 mx-2" onSubmit={handleFormSubmit}>
+                    <div className="grid grid-cols-2">
                         <label className="font-bold">Name</label>
                         <input
                             className="border-2"
@@ -62,7 +62,7 @@ function Todoform({ fetchTasks }: { fetchTasks: () => void }) {
                             value={data.title}
                         />
                     </div>
-                    <div className="flex gap-14">
+                    <div className="grid grid-cols-2">
                         <label className="font-bold">LandType</label>
                         <select className='border-2 p-2' name='landType' onChange={handleChange} value={data.landType}>
                             <option value="">Select Land Type</option>
@@ -71,7 +71,7 @@ function Todoform({ fetchTasks }: { fetchTasks: () => void }) {
                             <option value="Commercial">Commercial</option>
                         </select>
                     </div>
-                    <div className="flex gap-14">
+                    <div className="grid grid-cols-2">
                         <label className="font-bold">LandSize(acres)</label>
                         <input
                             className="border-2"
@@ -82,7 +82,7 @@ function Todoform({ fetchTasks }: { fetchTasks: () => void }) {
                             value={data.landSize}
                         />
                     </div>
-                    <div className="flex gap-14">
+                    <div className="grid grid-cols-2">
                         <label className="font-bold">Price</label>
                         <input
                             className="border-2"
@@ -93,7 +93,7 @@ function Todoform({ fetchTasks }: { fetchTasks: () => void }) {
                             value={data.price}
                         />
                     </div>
-                    <div className="flex gap-14">
+                    <div className="grid grid-cols-2">
                         <label className="font-bold">Phone</label>
                         <input
                             className="border-2"
@@ -104,30 +104,32 @@ function Todoform({ fetchTasks }: { fetchTasks: () => void }) {
                             value={data.phone}
                         />
                     </div>
-                    <div className="flex gap-14">
+                    <div className="grid grid-cols-2">
                         <label className='font-bold'>Irrigation</label>
-                        <div>
-                            <input
-                                type='radio'
-                                className='border-2'
-                                name='irrigation'
-                                value='true'
-                                onChange={handleChange} // Handle change event to update data.irrigation
-                            />
-                            <label>Yes</label>
-                        </div>
-                        <div>
-                            <input
-                                type='radio'
-                                className='border-2'
-                                name='irrigation'
-                                value='false'
-                                onChange={handleChange} // Handle change event to update data.irrigation
-                            />
-                            <label>No</label>
+                        <div className='flex gap-5'>
+                            <div>
+                                <input
+                                    type='radio'
+                                    className='border-2'
+                                    name='irrigation'
+                                    value='true'
+                                    onChange={handleChange} // Handle change event to update data.irrigation
+                                />
+                                <label>Yes</label>
+                            </div>
+                            <div>
+                                <input
+                                    type='radio'
+                                    className='border-2'
+                                    name='irrigation'
+                                    value='false'
+                                    onChange={handleChange} // Handle change event to update data.irrigation
+                                />
+                                <label>No</label>
+                            </div>
                         </div>
                     </div>
-                    <div className="mt-5 flex gap-10">
+                    <div className="grid grid-cols-2">
                         <label className="font-bold">Address</label>
                         <textarea
                             rows={3}
--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -50,9 +50,9 @@ function Editpage() {
     }
   }
   return (
-    <div className='flex flex-col items-center mt-10 mx-auto'>
-            <form className="mt-5 shadow-inner bg-white sm:p-10 px-5 py-5 flex flex-col gap-5" onSubmit={handleEdit}>
-                    <div className="flex gap-14">
+    <div className='flex flex-col items-center mt-5 '>
+            <form className="mt-5 shadow-inner bg-white sm:p-10  py-5 flex flex-col gap-5 px-5 mx-2 mb-10" onSubmit={handleEdit}>
+                    <div className="grid grid-cols-2">
                         <label className="font-bold">Name</label>
                         <input
                             className="border-2"
@@ -63,7 +63,7 @@ function Editpage() {
                             value={editdata.title}
                         />
                     </div>
-                    <div className="flex gap-14">
+                    <div className="grid grid-cols-2">
                         <label className="font-bold">LandType</label>
                         <select className='border-2 p-2' name='landType' onChange={handleChange} value={editdata.landType}>
                             <option value="">Select Land Type</option>
@@ -72,7 +72,7 @@ function Editpage() {
                             <option value="Commercial">Commercial</option>
                         </select>
                     </div>
-                    <div className="flex gap-14">
+                    <div className="grid grid-cols-2">
                         <label className="font-bold">LandSize(acres)</label>
                         <input
                             className="border-2"
@@ -83,7 +83,7 @@ function Editpage() {
                             value={editdata.landSize}
                         />
                     </div>
-                    <div className="flex gap-14">
+                    <div className="grid grid-cols-2">
                         <label className="font-bold">Price</label>
                         <input
                             className="border-2"
@@ -94,7 +94,7 @@ function Editpage() {
                             value={editdata.price}
                         />
                     </div>
-                    <div className="flex gap-14">
+                    <div className="grid grid-cols-2">
                         <label className="font-bold">Phone</label>
                         <input
                             className="border-2"
@@ -105,32 +105,34 @@ function Editpage() {
                             value={editdata.phone}
                         />
                     </div>
-                    <div className="flex gap-14">
+                    <div className="grid grid-cols-2">
                         <label className='font-bold'>Irrigation</label>
-                        <div>
-                            <input
-                                type='radio'
-                                className='border-2'
-                                name='irrigation'
-                                value='true'
-                                checked={editdata.irrigation === true} // Set checked if data.irrigation is true
-                                onChange={handleChange} // Handle change event to update data.irrigation
-                            />
-                            <label>Yes</label>
-                        </div>
-                        <div>
-                            <input
-                                type='radio'
-                                className='border-2'
-                                name='irrigation'
-                                value='false'
-                                checked={editdata.irrigation === false} // Set checked if data.irrigation is false
-                                onChange={handleChange} // Handle change event to update data.irrigation
-                            />
-                            <label>No</label>
+                        <div className='flex gap-2'>
+                            <div>
+                                <input
+                                    type='radio'
+                                    className='border-2'
+                                    name='irrigation'
+                                    value='true'
+                                    checked={editdata.irrigation === true} // Set checked if data.irrigation is true
+                                    onChange={handleChange} // Handle change event to update data.irrigation
+                                />
+                                <label>Yes</label>
+                            </div>
+                            <div>
+                                <input
+                                    type='radio'
+                                    className='border-2'
+                                    name='irrigation'
+                                    value='false'
+                                    checked={editdata.irrigation === false} // Set checked if data.irrigation is false
+                                    onChange={handleChange} // Handle change event to update data.irrigation
+                                />
+                                <label>No</label>
+                            </div>
                         </div>
                     </div>
-                    <div className="mt-5 flex gap-10">
+                    <div className="grid grid-cols-2">
                         <label className="font-bold">Address</label>
                         <textarea
                             rows={3}
